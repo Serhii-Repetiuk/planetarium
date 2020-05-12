73fffb410e31857a80a72e0cc4736dc7ffc03218
вашÐserhii <serhii.repetiuk@gmail.com>
2020-05-12 21:56:33 +0300

    feat: Довавил стили в работу акордеона и в интро

--- source/js/components/accordion.js
+++ source/js/components/accordion.js
@@ -1,8 +1,8 @@
-const questionBar = document.getElementsByClassName('question__container');
+const questionBar = document.querySelectorAll('.question__container');
 
 for (let i = 0; i < questionBar.length; i++) {
   questionBar[i].addEventListener('click', function handler() {
-    this.classList.toggle('active');
+    this.querySelector('.question__button-img').classList.toggle('question__button-img--active');
     const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
