0799ea141b0eb37009c03d06db8b24bdb3a949f6
вашÐserhii <serhii.repetiuk@gmail.com>
2020-06-21 02:12:04 +0300

    feat: Добавил ховеры

---
+++ source/js/components/common.js
@@ -0,0 +1,20 @@
+let orderButton = document.querySelector('.order-popup');
+let orderForm = document.querySelector('.order');
+let close = document.querySelector('.order-form__close');
+let modalShadow = document.querySelector('.modal-shadow');
+
+orderButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  orderForm.classList.remove('visually-hidden');
+  orderForm.classList.add('modal-show');
+  modalShadow.classList.remove('visually-hidden');
+});
+
+
+close.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  orderForm.classList.add('visually-hidden');
+  orderForm.classList.remove('modal-show');
+  modalShadow.classList.add('visually-hidden');
+});
+
